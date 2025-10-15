27353965ba4a08ea11ee520b8570e5eee0c33438
mitsusaburo <bschindlerm@gmail.com>
2025-10-15 19:29:03 -0300

    Improve rockpaperscissors, add await function

--- javascript_tutorial/Rock paper scissors/script.js
+++ javascript_tutorial/Rock paper scissors/script.js
@@ -1,38 +1,56 @@
-console.log("Scripts are working");
-
-let myChoice = "";
-
 const choices = ['rock', 'paper', 'scissors'];
+const winningMoves = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+};
 
-console.log(choices.length)
+
+const choicesDisplay = document.querySelector(".choices-display")
+// const computerChoiceDisplay = document.querySelector(".computer-choice")
+const result = document.querySelector(".result")
+let winCounter = 0;
+let loseCounter = 0;
+const winDisplay = document.querySelector(".win-display")
+const loseDisplay = document.querySelector(".lose-display")
 
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function runGame(myChoice) {
+
+async function runGame(myChoice) {
+    result.style.color = 'black';
+    result.innerHTML = "Result:";
+    
+    choicesDisplay.innerHTML = `${myChoice} vs ...`;
+
+    await delay(1000);
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    choicesDisplay.innerHTML = `${myChoice} vs ${computerChoice}`;
 
 
-    const myChoiceDisplay = document.querySelector(".my-choice")
-    const computerChoiceDisplay = document.querySelector(".computer-choice")
-    const result = document.querySelector(".result")
-
-    myChoiceDisplay.innerHTML = `Me: ${myChoice}`;
-    computerChoiceDisplay.innerHTML = `PC: ${computerChoice}`;
-
-
-    const winningMoves = {
-        rock: 'scissors',
-        paper: 'rock',
-        scissors: 'paper'
-    };
-
-
+    await delay(1000);
     if (myChoice === computerChoice) {
+        result.style.color = 'black';
         result.innerHTML = "Result: Tie";
+
     } else if (winningMoves[myChoice] === computerChoice) {
-        result.innerHTML = "Result: I win";
+        result.style.color = 'darkgreen';
+        result.innerHTML = "Result: YOU WIN"
+        winCounter++;
+        // await delay(500);
+        winDisplay.innerHTML = `Wins: ${winCounter}`;
     } else {
-        result.innerHTML = "Result: Computer wins";
+        result.style.color = 'darkred';
+        result.innerHTML = "Result: YOU LOSE"
+        loseCounter++;
+        // await delay(500);
+        loseDisplay.innerHTML = `Loses: ${loseCounter}`
     }
 
 }
+
+
+
---
+++ javascript_tutorial/test2/script.js
@@ -0,0 +1,16 @@
+
+// CONDITIONAL OPERATORS
+/*
+let a = 3
+let b = 2
+const result = (a > b) ? 'higher' : 'lower'
+console.log(result)
+
+let c = 3
+let d = 2
+c > d && console.log('Higher')
+
+let currency = ""
+let paymentCurrency = currency || 'USD'
+console.log('payment currency:', paymentCurrency)
+*/
