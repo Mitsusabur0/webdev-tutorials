9a7212ae711271df5c7fb6bb4e3ff330b5f898e6
mitsusaburo <bschindlerm@gmail.com>
2025-10-22 16:38:18 -0300

    Advance on todolist, event delegation op

--- javascript_tutorial/to-do list/script.js
+++ javascript_tutorial/to-do list/script.js
@@ -1,37 +1,33 @@
-// const todoList = []
-
 const inputElement = document.querySelector(".js-name-input");
 const inputDate = document.querySelector(".js-date-input");
-const buttonAdd = document.querySelector(".btn-add")
-const listContainer = document.querySelector(".grid-container");
+const buttonAdd = document.querySelector(".btn-add");
+const listContainer = document.querySelector(".container");
 
-// buttonAdd.addEventListener('click', () => {
-//     const newElement = document.createElement('p');
-//     newElement.textContent = inputElement.value;
-//     listContainer.appendChild(newElement);
-//     inputElement.value = "";
-
-// })
-
-const newRow = document.createElement('div')
-newRow.classList.add('list-row')
 
 
 buttonAdd.addEventListener('click', () => {
-    listContainer.appendChild(newRow);
-    const elementName = document.createElement('p');
-    elementName.textContent = inputElement.value;
 
-    const elementDate = document.createElement('p')
-    elementDate.textContent = inputDate.value;
+    const newContainer = document.createElement('div');
+    const newElement = document.createElement('p')
+    const newElementDate = document.createElement('p')
+    const newButtonDelete = document.createElement('button')
+    
+    newContainer.classList.add('grid-container');
+    newElement.textContent = inputElement.value;
+    newElementDate.textContent = inputDate.value;
+    newButtonDelete.textContent = 'Delete';
+    newButtonDelete.classList.add('btn-delete')
+    
+    newContainer.append(newElement, newElementDate, newButtonDelete)
+    listContainer.append(newContainer);
 
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Delete'
-    buttonDelete.classList.add("btn-delete");
+    inputElement.value = '';
+    // inputDate.value = '';
+})
 
-    listContainer.append(elementName)
-    listContainer.append(elementDate)
-    listContainer.append(buttonDelete)
-
-
+listContainer.addEventListener('click', (event) => {
+    console.log('clicked')
+    if (event.target.classList.contains('btn-delete')) {
+        event.target.closest('.grid-container').remove()
+    }
 })
