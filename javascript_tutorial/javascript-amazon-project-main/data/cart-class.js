42d382424bb80e39f78d64d2971faee166b08891
mitsusaburo <bschindlerm@gmail.com>
2025-10-31 14:33:08 -0300

    Add private properties and methods to cart-class

--- javascript_tutorial/javascript-amazon-project-main/data/cart-class.js
+++ javascript_tutorial/javascript-amazon-project-main/data/cart-class.js
@@ -1,17 +1,18 @@
 class Cart {
     // Class Properties
     cartItems;
-    localStorageKey;
+    // # = private property
+    #localStorageKey;
 
     // Code that runs when creating a new instance of the class
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
     // Class Methods
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
         if (!this.cartItems) {
             this.cartItems = [
             {
@@ -28,7 +29,7 @@ class Cart {
     }
 
     saveCartStorage() {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
 
     addToCart(productId, quantity) {
@@ -75,8 +76,5 @@ const businessCart = new Cart('cart-business');
 
 
 
-
 console.log(cart);
 console.log(businessCart);
-
-console.log(businessCart instanceof Cart);
